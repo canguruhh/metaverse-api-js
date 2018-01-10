76e0437c89ca7c814273add6c5f4d983c3fe8654
Sven <sven@mutzl.org>
2018-01-10 08:28:11 +0000

    Test options

--- src/options.spec.ts
+++ src/options.spec.ts
@@ -5,15 +5,16 @@ describe('Config options', () => {
     var options = new Options('127.0.0.1', 8820,  'http' );
     describe('getHost', () => {
         it('getHost', () => {
-            const result = new Options( '127.0.0.1', 8820, 'http').getHost()
-            expect(result).to.equal('127.0.0.1');
+            expect(options.getHost()).to.equal('127.0.0.1');
         });
     });
     it('get protocol', () => {
         expect(options.getProtocol()).to.equal('http');
     });
     it('get port', () => {
-        const result = options.getPort()
-        expect(result).to.equal(8820);
+        expect(options.getPort()).to.equal(8820);
+    });
+    it('get url', () => {
+        expect(options.getUrl()).to.equal('http://127.0.0.1:8820');
     });
 });
